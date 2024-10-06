de42723d95b01f85d880e2834050ca481d99b1c4
guswlda <guswl71945@gmail.com>
2024-10-06 12:26:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(
   cors({
     // origin: 'http://localhost:3000',
     origin: 'https://myplanner.guswldaiccproject.com',
+    methods: 'GET,POST,PUT,DELETE',
     credentials: true,
   })
 );
