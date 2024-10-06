851601d749577ddbb4b21c26f6bdb715d036f43b
guswlda <guswl71945@gmail.com>
2024-10-06 21:37:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: [
-      'http://localhost:3000',  // 로컬 테스트용 도메인
+      'http://localhost:3001',  // 로컬 테스트용 도메인
       'https://myplanner.guswldaiccproject.com' // 프로덕션 도메인
     ],
     credentials: true,
